8783044315daa5cf3d4037ace9f87bcc8a31d254
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-12 10:52:07 -0300

    Estrutura inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,40 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  
+  /*Reset CSS */
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+  
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family:sans-serif;
+    background-color:#D9E6F6;
   }
-`
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +44,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
