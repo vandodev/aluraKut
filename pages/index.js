a2a9ee1c71025701442955a141795c5d9295488f
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-12 11:12:29 -0300

    :satisfied: Separando components

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,5 @@
-import styled from "styled-components";
-
-const Box = styled.div`
-  background: #ffff;
-  border-radius: 8px;
-`;
-
-const MainGrid = styled.main`
-  width: 100%;
-  grid-gap: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 500px;
-  padding: 16px;
-  .profileArea {
-    display: none;
-    @media (min-width: 860px) {
-      display: block;
-    }
-  }
-  @media (min-width: 860px) {
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
-  }
-`;
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
 
 export default function Home() {
   return (
---
+++ src/components/Box/index.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+const Box = styled.div`
+  background: #ffff;
+  border-radius: 8px;
+`;
+export default Box;
