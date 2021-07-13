daed954e3e2eb08e8172e1da4e57982f400b1692
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-13 00:26:37 -0300

    :fire: Estrutura do formulário

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,33 @@ export default function Home() {
             <h1 className="title">Bem-Vindo(a)</h1>
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form
+              onSubmit={function handleCriaComunidade(e) {
+                e.preventDefault();
+                console.log(e);
+              }}
+            >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+
+              <button>Criar comunidade</button>
+            </form>
+          </Box>
         </div>
         <div
           className="profileRelationsArea"
